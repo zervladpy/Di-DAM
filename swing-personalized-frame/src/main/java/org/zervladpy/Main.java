ae351797c73eb8482fd465fabdb9aaa428a9aa30
zervladpy <zernovvladislav7@gmail.com>
2023-12-11 22:07:40 +0100

    -a `swing-personalized-frame`

---
+++ swing-personalized-frame/src/main/java/org/zervladpy/Main.java
@@ -0,0 +1,6 @@
+package org.zervladpy;
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(MainFrame::new);
+    }
+}
---
+++ swing-personalized-frame/src/main/java/org/zervladpy/MainFrame.java
@@ -0,0 +1,18 @@
+package org.zervladpy;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        super("New Personalized Frame");
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        setSize(500, 600);
+
+        setVisible(true);
+    }
+
+}
